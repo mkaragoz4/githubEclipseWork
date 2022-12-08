bdf98e28d0b7506d2ae26ea1eb9af1cd11419fb9
M.Ferhat KARAGÖZ <mkaragoz4@gmail.com>
2022-12-08 14:45:17 +0100

    ilk commit

---
+++ githubEclipseWork/src/githubEclipseWork/GithubHw1.java
@@ -0,0 +1,11 @@
+package githubEclipseWork;
+
+public class GithubHw1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Git");
+	}
+
+}
